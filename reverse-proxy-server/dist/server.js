f1c503a43008a5539ecc37f24c931fb0efead861
Rehman Khan <rehman.khan.300a@gmail.com>
2024-08-14 01:07:02 +0530

    added proxy-server to serve the deployed files from S3

---
+++ reverse-proxy-server/dist/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import httpProxy from 'http-proxy';
+import dotenv from 'dotenv';
+dotenv.config();
+const app = express();
+const PORT = 8000;
+const BASE_PATH = process.env.S3_BUCKET_FOLDER_URL;
+const proxy = httpProxy.createProxyServer();
+app.use((req, res) => {
+    const hostname = req.hostname;
+    const subdomain = hostname.split('.')[0];
+    console.log('Subdomain:', subdomain);
+    const resolvesTo = `${BASE_PATH}/${subdomain}`;
+    proxy.web(req, res, { target: resolvesTo, changeOrigin: true });
+});
+// @ts-ignore: Ignore TypeScript type checking for this block
+proxy.on('proxyReq', (proxyReq, req, res) => {
+    const url = req.url;
+    if (url === '/') {
+        proxyReq.path += 'index.html';
+    }
+});
+app.listen(PORT, () => console.log(`Reverse Proxy Running on port ${PORT}`));
---
+++ reverse-proxy-server/src/server.ts
@@ -0,0 +1,32 @@
+import express, { Request, Response } from 'express';
+import httpProxy from 'http-proxy';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = 8000;
+
+const BASE_PATH = process.env.S3_BUCKET_FOLDER_URL;
+
+const proxy = httpProxy.createProxyServer();
+
+app.use((req: Request, res: Response) => {
+    const hostname = req.hostname;
+    const subdomain = hostname.split('.')[0];
+    console.log('Subdomain:', subdomain);
+
+    const resolvesTo = `${BASE_PATH}/${subdomain}`;
+
+    proxy.web(req, res, { target: resolvesTo, changeOrigin: true });
+});
+
+// @ts-ignore: Ignore TypeScript type checking for this block
+proxy.on('proxyReq', (proxyReq, req, res) => {
+    const url = req.url;
+    if (url === '/') {
+        proxyReq.path += 'index.html';
+    }
+});
+
+app.listen(PORT, () => console.log(`Reverse Proxy Running on port ${PORT}`));
