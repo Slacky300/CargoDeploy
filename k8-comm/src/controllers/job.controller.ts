5505965be45b2d85720fb940344ed0fdde6972e2
Rehman Khan <rehman.khan.300a@gmail.com>
2025-01-06 23:57:52 +0530

    update k8 and job controller

--- k8-comm/src/controllers/job.controller.ts
+++ k8-comm/src/controllers/job.controller.ts
@@ -4,10 +4,10 @@ import { createJob } from "../utils/k8job.js";
 import { sendResponse } from "../utils/sendResponse.js";
 
 export const createProject = expressAsyncHandler(async (req: Request, res: Response) => {
-    const { git_url, project_id, root_folder, env_variables, name, access_token, branch, deploymentId } = req.body; 
+    const { git_url, project_id, root_folder, env_variables, name, access_token, branch, deploymentId, email } = req.body; 
     console.log(env_variables)
 
-    const stats = await createJob(git_url, project_id, root_folder, env_variables, branch, deploymentId,access_token, name); 
+    const stats = await createJob(git_url, project_id, root_folder, env_variables, branch, deploymentId,email, access_token, name); 
     //send an email to the user
     return sendResponse(res, 200, {message: `Job completed successfully`, data: stats});
 });
