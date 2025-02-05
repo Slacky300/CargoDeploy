7d3be64cf77c7dee5cd1e7de0410cb87ec130c73
Rehman Khan <rehman.khan.300a@gmail.com>
2025-02-05 20:10:30 +0530

    bugs fixed for createJob

--- k8-comm/src/controllers/job.controller.ts
+++ k8-comm/src/controllers/job.controller.ts
@@ -5,7 +5,6 @@ import { sendResponse } from "../utils/sendResponse.js";
 
 export const createProject = expressAsyncHandler(async (req: Request, res: Response) => {
     const { git_url, project_id, root_folder, env_variables, name, access_token, branch, deployment_id } = req.body; 
-    console.log(req.body);
     createJob(git_url, project_id, root_folder, JSON.parse(env_variables), branch, deployment_id, "rehman.khan.300a@gmail.com", access_token, name); 
     sendResponse(res, 200, {message: `Job completed successfully`, data: "Job created successfully"});
 
