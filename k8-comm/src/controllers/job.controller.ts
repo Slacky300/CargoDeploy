d6b3aaa8e4eeb9006d14855e80bb134ac86c8352
Rehman Khan <rehman.khan.300a@gmail.com>
2025-03-25 16:34:25 +0530

    removed unwanted deployment status update

--- k8-comm/src/controllers/job.controller.ts
+++ k8-comm/src/controllers/job.controller.ts
@@ -5,13 +5,6 @@ import { sendResponse } from "../utils/sendResponse.js";
 
 export const createProject = expressAsyncHandler(async (req: Request, res: Response) => {
     const { git_url, project_id, root_folder, env_variables, name, access_token, branch, deployment_id, build_command, install_command, commit_sha, email } = req.body;
-    await fetch(`${process.env.FRONTEND_URL}/api/deployment?deploymentIdWithStatus=${deployment_id}-${"IN_PROGRESS"}`, {
-        method: "PATCH",
-        headers: {
-            "Content-Type": "application/json",
-            "x-api-key": `${process.env.CLERK_KEY}`
-        },
-    }); 
     createJob(git_url, project_id, root_folder, JSON.parse(env_variables), branch, deployment_id, email, build_command, install_command, commit_sha, access_token, name); 
     sendResponse(res, 200, {message: `Job completed successfully`, data: "Job created successfully"});
 
